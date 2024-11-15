f962b0b7405053c7a05ef27b803421e711b566bc
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-15 15:18:01 +0100

    ADD names function

--- names.js
+++ names.js
@@ -1,4 +1,8 @@
 const firstName = process.argv[2];
 const lastName = process.argv[3];
 
-console.log(firstName, lastName);
+function names(firstName, lastName) {
+  console.log(firstName, lastName);
+}
+
+names(firstName, lastName);
