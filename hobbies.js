b3e8a1a8c1b80c0547be90031f94c2ec119a1092
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-15 15:34:25 +0100

    ADD hobbiesList funcion

--- hobbies.js
+++ hobbies.js
@@ -3,3 +3,12 @@ const hobbyTwo = process.argv[3];
 const hobbyThree = process.argv[4];
 
 // console.log(hobbyOne, hobbyTwo, hobbyThree);
+
+function hobbiesList(hobbyOne, hobbyTwo, hobbyThree) {
+  const listOfHobbies = {
+    hobbies: ["hobbyOne", "hobbyTwo", "hobbyThree"],
+  };
+  return listOfHobbies;
+}
+
+console.log(hobbiesList());
